b0165ffc920aa577199be29abc5a0c65ab175213
Specthildur <164398996+Specthildur@users.noreply.github.com>
2025-05-01 23:49:12 +0100

    initial view load

--- app/services/searchFunction.js
+++ app/services/searchFunction.js
@@ -1,40 +1,52 @@
 "use server";
 
 export async function searchFunction(searchValue, type) {
-    if (type === "params") {
-        const response = await fetch(
-            `https://api.deezer.com/search/album/${searchValue}`,
-            {
-                method: "GET",
-            }
-        );
-        if (!response.ok) {
-            const error = new Error("Error fetching search query");
-            error.code = response.status;
-            error.info = await response.json();
-            throw error;
-        }
-        const data = await response.json();
-        const results = await data.data;
-        return results;
+  console.log(searchValue);
+  if (type === "params") {
+    const response = await fetch(
+      `https://api.deezer.com/search/album/${searchValue}`,
+      {
+        method: "GET",
+      }
+    );
+    if (!response.ok) {
+      const error = new Error("Error fetching search query");
+      error.code = response.status;
+      error.info = await response.json();
+      throw error;
     }
-
-    if (type === "album") {
-        const response = await fetch(
-            `https://api.deezer.com/search/album?q=${searchValue}`,
-            {
-                method: "GET",
-            }
-        );
-
-        if (!response.ok) {
-            const error = new Error("Error fetching search query");
-            error.code = response.status;
-            error.info = await response.json();
-            throw error;
-        }
-        const data = await response.json();
-        const results = await data.data;
-        return results;
+    const data = await response.json();
+    const results = await data.data;
+    return results;
+  } else if (searchValue === "all") {
+    const response = await fetch("https://api.deezer.com/chart/0/albums", {
+      method: "GET",
+    });
+    if (!response.ok) {
+      const error = new Error("Error fetching search query");
+      error.code = response.status;
+      error.info = await response.json();
+      throw error;
     }
+    const data = await response.json();
+    const results = await data.data;
+    return results;
+  } else if (type === "album") {
+    const response = await fetch(
+      `https://api.deezer.com/search/album?q=${searchValue}`,
+      {
+        method: "GET",
+      }
+    );
+
+    if (!response.ok) {
+      const error = new Error("Error fetching search query");
+      error.code = response.status;
+      error.info = await response.json();
+      throw error;
+    }
+    const data = await response.json();
+    const results = await data.data;
+    return results;
+  }
 }
