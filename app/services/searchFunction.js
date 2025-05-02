60d148c5f22b38b1263986c08101b3e40fbe071c
Specthildur <164398996+Specthildur@users.noreply.github.com>
2025-05-02 23:43:07 +0100

    search cleanup fail...

--- app/services/searchFunction.js
+++ app/services/searchFunction.js
@@ -49,3 +49,37 @@ export async function searchFunction(searchValue, type) {
     return results;
   }
 }
+
+// export async function searchFunction(searchValue, param) {
+//   try {
+//     if (param === "album" && searchValue !== "all") {
+//       const response = await fetch(
+//         `https://api.deezer.com/search/album?q=${searchValue}`
+//       );
+//       const data = await response.json();
+//       if (!data) throw new Error("No response found");
+//       const results = await data.data;
+//       return results;
+//     } else if (
+//       (searchValue === "all" || searchValue === undefined) &&
+//       param === "album"
+//     ) {
+//       const response = await fetch("https://api.deezer.com/chart/0/albums");
+//       const data = await response.json();
+//       if (!data) throw new Error("No response found");
+//       const results = await data.data;
+//       return results;
+//     } else if (param === "params") {
+//       const response = await fetch(
+//         `https://api.deezer.com/search/album/${searchValue}`
+//       );
+//       const data = await response.json();
+//       if (!data) throw new Error("No response found");
+//       const results = await data.data;
+//       return results;
+//     }
+//   } catch (error) {
+//     const data = error.data;
+//     return data;
+//   }
+// }
