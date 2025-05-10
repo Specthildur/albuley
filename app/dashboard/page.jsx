328e27cc2bf105e78be07479cb221cfb895bc02d
Specthildur <164398996+Specthildur@users.noreply.github.com>
2025-05-10 18:36:36 +0100

    further changes

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,26 +5,22 @@ import { auth } from "@/app/utils/auth";
 import { headers } from "next/headers";
 import MainContent from "@/components/MainContent/MainContent";
 import { redirect } from "next/navigation";
-import { createUser } from "../utils/create-data";
 
 export default async function HomePage() {
-    const session = await auth.api.getSession({
-        headers: await headers(),
-    });
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-    const response = await createUser();
-    console.log(response);
-
-    // if (!session) {
-    //   redirect("/sign-up");
-    // } else {
-    return (
-        <>
-            <Header base={true}></Header>
-            <div className="mx-[5%] h-max flex">
-                <Filter></Filter>
-                <MainContent></MainContent>
-            </div>
-        </>
-    );
+  // if (!session) {
+  //   redirect("/sign-up");
+  // } else {
+  return (
+    <>
+      <Header base={true}></Header>
+      <div className="mx-[5%] h-max flex">
+        <Filter></Filter>
+        <MainContent></MainContent>
+      </div>
+    </>
+  );
 }
--- app/utils/sign-up.js
+++ app/utils/sign-up.js
@@ -1,24 +1,36 @@
 "use server";
 import { auth } from "@/app/utils/auth";
+import createUserData from "./create-user-data";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function signUp(formData) {
-    const name = formData.get("full-name");
-    const email = formData.get("email");
-    const password = formData.get("password");
-    try {
-        await auth.api.signUpEmail({
-            body: {
-                name: name,
-                email: email,
-                password: password,
-                role: "user",
-            },
-        });
-        return { error: null };
-    } catch (err) {
-        return err.body;
+  const name = formData.get("full-name");
+  const email = formData.get("email");
+  const password = formData.get("password");
+  try {
+    const signedUpUser = await auth.api.signUpEmail({
+      body: {
+        name: name,
+        email: email,
+        password: password,
+        role: "user",
+      },
+    });
+    console.log(signedUpUser);
+    const createdData = await createUserData();
+    console.log(createdData);
+
+    if (!createdData) {
+      throw new Error("User is already created");
     }
+    return { error: null };
+  } catch (err) {
+    if (err.body) {
+      return err.body;
+    } else {
+      return err;
+    }
+  }
 }
